2e029da71bdd93cd1eadb6fc68ffaf8f5008eeed
ArnaudDarre <arnaudarre@gmail.com>
2025-07-10 11:46:08 +0800

    (react) Remove Link import from NavBar

--- packages/pallote-react/src/components/NavBar.jsx
+++ packages/pallote-react/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import { Section } from './Section'
 
