fab6af58701832666759a7b812b3cdce21753b23
Arnaud Darré <arnaudarre@gmail.com>
2026-03-08 15:59:50 +0800

    feat(react): add titleComponent prop to CardHeader, close mobile menu on link click via event delegation

--- packages/pallote-react/src/components/CardHeader.tsx
+++ packages/pallote-react/src/components/CardHeader.tsx
@@ -3,16 +3,19 @@ import classnames from 'classnames'
 
 import { Text } from '../utilities/Text'
 
+type CardHeaderTitleComponent = 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
 export interface CardHeaderProps extends HTMLAttributes<HTMLDivElement> {
   icon?: ReactElement<{ className?: string }>
   label?: string
   title: string
+  titleComponent?: CardHeaderTitleComponent
   subtitle?: React.ReactNode
   actions?: React.ReactNode
   className?: string
 }
 
-export const CardHeader = ({ icon, label, title, subtitle, actions, className, ...props }: CardHeaderProps) => {
+export const CardHeader = ({ icon, label, title, titleComponent = 'h3', subtitle, actions, className, ...props }: CardHeaderProps) => {
   return (
     <div
       className={classnames([
@@ -28,7 +31,7 @@ export const CardHeader = ({ icon, label, title, subtitle, actions, className, .
       )}
       {icon ? React.cloneElement(icon, { className: `${icon.props.className ?? ''} card_icon` }) : null}
       {label ? (<Text className={classnames('card_label')}>{label}</Text>) : null}
-      <Text className={classnames('card_title')}>{title}</Text>
+      <Text component={titleComponent} className={classnames('card_title')}>{title}</Text>
       {subtitle ? (<Text className={classnames('card_subtitle')}>{subtitle}</Text>) : null}
     </div>
   )
